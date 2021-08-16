5c5432d1013abddad11e6e02b83591a1bca02d85
Abousoufian <11903685@student.pxl.be>
2021-08-16 11:46:32 +0200

    added add_data.php file

---
+++ add_data.php
@@ -0,0 +1,9 @@
+
+<?php
+    // Connect to MySQL
+    include("connect.php");
+
+    //mysqli_query($conn, "insert into Data_Tabel (temperature, humidity) values ('".$_GET["temp"]."','".$_GET["hum"]."')");
+    mysqli_query($conn, "insert into Data_Tabel (sensorID, Value) values ('1', '".$_GET["temp"]."')");
+    mysqli_query($conn, "insert into Data_Tabel (sensorID, Value) values ('2', '".$_GET["hum"]."')");
+?>
