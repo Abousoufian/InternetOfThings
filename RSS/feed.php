9c6f229df1c21e80ca73ea1ae8b23793fe83ae6d
Abousoufian <11903685@student.pxl.be>
2021-08-30 15:14:37 +0200

    update

--- RSS/feed.php
+++ RSS/feed.php
@@ -8,24 +8,23 @@
 	$stmtMM = $conn->prepare("Select MIN(Value), MAX(Value) FROM Data_Tabel Where SensorID = ?");
 	$stmtAVG = $conn->prepare("Select FORMAT(AVG(Value),2) From Data_Tabel WHERE Time >= now() - INTERVAL 1 DAY AND SensorID = ?");
 	$stmtLAST = $conn->prepare("Select Value From Data_Tabel WHERE SensorID = ? ORDER BY Time DESC LIMIT 1");
-	$stmtNAME = $conn->prepare("Select Name FROM Sensoren_Tabel WHERE ID = ?");
+    $stmtNAME = $conn->prepare("Select SensorName FROM Sensoren_Tabel WHERE ID = ?");
 	
 	$stmtMM->bind_param('i',$i);
 	$stmtAVG->bind_param('i',$i);
 	$stmtLAST->bind_param('i',$i);
-	$stmtNAME->bind_param('i',$i);
+    $stmtNAME->bind_param('i',$i);
 	
 	header( "Content-type: text/xml");
 	
 	echo "<?xml version='1.0' encoding='UTF-8'?>
 	<rss version='2.0'>
-    
+
 	<channel>
 	<title>Sensor Statistics</title>
-	<link>$link</link>
 	<description>RSS feed Internet of Things</description>";
 	
-	for($i = 1; $i < 4; $i ++)
+	for($i = 1; $i < 3; $i ++)
 	{
 		$stmtMM->execute();
 		$stmtMM->store_result();
@@ -41,18 +40,17 @@
 		$stmtLAST->store_result();
 		$stmtLAST->bind_result($value[0]);
 		$stmtLAST->fetch();
-		
-		$stmtNAME->execute();
-		$stmtNAME->store_result();
-		$stmtNAME->bind_result($name);
-		$stmtNAME->fetch();
+
+        $stmtNAME->execute();
+        $stmtNAME->store_result();
+        $stmtNAME->bind_result($name);
+        $stmtNAME->fetch();
 		
 		for ($j = 0; $j < 4; $j++)
 		{
 			echo "<item>
-						<category> $name </category>
+                        <category> $name </category>
 						<title> $title[$j] </title>
-						<link>$link</link>
 						<description>$value[$j]</description>
 					</item>";
 		}
